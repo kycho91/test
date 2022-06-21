a6625035fec3a088a7fa991a349fd9f117c2da36
kycho91 <kycho91@lguplus.co.kr>
2022-06-21 10:38:58 +0900

    chore: 모듈 이름 변경(domain-sample => domain)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "service-dev-backend-boilerplate"
 
 include("api")  // application
-include("domain-sample")   // domain
+include("domain")   // domain
 include("api-common") // infra
 include("infra-rds") // infra
