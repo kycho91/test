3af5e1e1e8af56817f3d1abdc66debed744affa3
kycho91 <kycho91@lguplus.co.kr>
2022-06-21 15:57:24 +0900

    feature: api-admin 모듈생성

---
+++ api-admin/src/main/kotlin/com/kidsworld/web/admin/ApiAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.kidsworld.web.admin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiAdminApplication
+
+fun main(args: Array<String>) {
+  runApplication<ApiAdminApplication>(*args)
+}
