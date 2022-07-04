7a884abb9d1366570562b4a0f8daea0246a8bc74
kycho91 <kycho91@lguplus.co.kr>
2022-07-04 15:24:28 +0900

    chore: api-amdin모듈에도 초기데이터 추가

---
+++ api-admin/src/main/resources/h2/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`id`, `email`, `name`, `tel`) VALUES (1, 'xxx@lguplus.co.kr', 'james', '010-0000-0001');
+INSERT INTO `user` (`id`, `email`, `name`, `tel`) VALUES (2, 'yyy@lguplus.co.kr', 'wookyun', '010-0000-0002');
+INSERT INTO `user` (`id`, `email`, `name`, `tel`) VALUES (3, 'yyy@lguplus.co.kr', 'seungkyu', '010-0000-0003');
---
+++ api-admin/src/main/resources/h2/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user(
+    id INT PRIMARY KEY,
+    email VARCHAR(30),
+    `name` VARCHAR(30),
+    tel VARCHAR(30)
+);
